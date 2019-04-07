1a9848ce61340b5b6575c7cc5c6a3c8f9e0196ad
Christophe <christophe_apollon@protonmail.com>
2019-04-07 16:31:02 -0400

    implémentation de ModifyStation

--- GenieK/src/station/Station.java
+++ GenieK/src/station/Station.java
@@ -7,12 +7,24 @@ public class Station {
     public void setName(String name){
         this.name = name;
     }
-
+    
+    public String getName(){
+        return this.name;
+    }
+    
     public void setId(String id) {
         this.id = id;
     }
-
+    
+    public String getId(){
+        return this.Id;
+    }
+    
     public void setCity(String city) {
         this.city = city;
+    }
+    
+    public String getCity(){
+        return this.City;
     }
 }
