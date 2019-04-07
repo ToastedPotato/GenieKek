435e7bb4795917047879c53c50f3efcbfcdc6da1
FahirahD <fahirah.diarra@gmail.com>
2019-04-07 17:13:43 -0400

    created Client class that implements the interface Visitor

---
+++ GenieK/src/visiteur/Client.java
@@ -0,0 +1,37 @@
+package visiteur;
+
+public class Client implements Visiteur{
+
+
+
+    public void visit (Company company){
+
+    }
+
+    public void visit (Place place){
+
+    }
+
+    public void visit (Station station){
+
+    }
+
+    public void visit (Transport transport){
+
+    }
+
+    public void visit (Schedule schedule){
+
+    }
+
+    public void visit (Trip trip){
+
+    }
+
+    public void visit (TripWithStop){
+
+    }
+
+
+
+}
