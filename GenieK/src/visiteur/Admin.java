a7985043cf96217ab0192bd70007f478872ac645
FahirahD <fahirah.diarra@gmail.com>
2019-04-07 17:10:53 -0400

    created Admin class that implements the interface Visiteur

---
+++ GenieK/src/visiteur/Admin.java
@@ -0,0 +1,35 @@
+package Visiteur;
+
+public class Client implements Visiteur{
+
+
+    public void visit (Company company){
+
+    }
+
+    public void visit (Place place){
+
+    }
+
+    public void visit (Station station){
+
+    }
+
+    public void visit (Transport transport){
+
+    }
+
+    public void visit (Schedule schedule){
+
+    }
+
+    public void visit (Trip trip){
+
+    }
+
+    public void visit (TripWithStop){
+
+    }
+
+
+}
